cfea0a0e0defcebd04f8f6ad3ee48673723fce41
jrwthll <manolov@dbis.rwth-aachen.de>
2015-04-09 17:51:54 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -138,13 +138,13 @@ docker -H tcp://0.0.0.0:7890 run --rm -v /var/run/docker.sock:/var/run/docker.so
 #Here, a default preconfiguration is done avoiding the CLI
 echo "Preparing custom configuration..." &&
 echo "Logging in Shipyard as admin ..." &&
-SHIPYARD_ADMIN_AUTH_TOKEN=$(curl -H "content-Type: application/json" -X POST -d \'{"username": "admin", "password": "shipyard"}\' http://localhost:8080/auth/login | jq \'.auth_token\') &&
+SHIPYARD_ADMIN_AUTH_TOKEN=$(curl -H "content-Type: application/json" -X POST -d '{"username": "admin", "password": "shipyard"}' http://localhost:8080/auth/login | jq '.auth_token') &&
 echo "... finished" &&
 echo "Changing admin password..." &&
-curl -H \'X-Access-Token: admin:$SHIPYARD_ADMIN_AUTH_TOKEN\' -X POST -d \'{"username": "admin", "password": "$SHIPYARD_ADMIN_PASS", "role": {"name": "admin"}}\' http://localhost:8080/api/accounts &&
+curl -H 'X-Access-Token: admin:$SHIPYARD_ADMIN_AUTH_TOKEN' -X POST -d '{"username": "admin", "password": "$SHIPYARD_ADMIN_PASS", "role": {"name": "admin"}}' http://localhost:8080/api/accounts &&
 echo "... finished" &&
 echo "Adding the Layers Box as the default Shipyard Engine..." &&
-curl -H \'X-Access-Token: admin:$SHIPYARD_ADMIN_AUTH_TOKEN\' -H \'Content-Type application/json\' -X POST -d \'{"id":"local", "ssl_cert": "", "ssl_key": "", "ca_cert": "", "engine": {"id": "local", "addr": "http://172.17.42.1:7890", "cpus": 3.0, "memory": 4096, "labels":["local"]}}\' http://localhost:8080/api/engines &&
+curl -H 'X-Access-Token: admin:$SHIPYARD_ADMIN_AUTH_TOKEN' -H 'Content-Type application/json' -X POST -d '{"id":"local", "ssl_cert": "", "ssl_key": "", "ca_cert": "", "engine": {"id": "local", "addr": "http://172.17.42.1:7890", "cpus": 3.0, "memory": 4096, "labels":["local"]}}' http://localhost:8080/api/engines &&
 echo "... finished" &&
 echo " -> done" &&
 echo "" &&
