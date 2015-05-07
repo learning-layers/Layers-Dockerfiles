d75c5ef732f4da3661da5ba8b935edd1e17776b8
Gordon Lawrenz <gordon.lawrenz@rwth-aachen.de>
2015-05-07 03:39:13 +0200

    Update init.sh

--- tomcat/src/main/files/init.sh
+++ tomcat/src/main/files/init.sh
@@ -3,15 +3,15 @@
 INITDIR="/opt/init/"
 
 if [ ! `ls -A ${INITDIR}` ]
-  then
-    echo "empty (or does not exist)"
-    return 1
-  fi
+	then
+		echo "empty (or does not exist)"
+		return 1
+fi
   
 for SCRIPT in ${INITDIR}*
 	do
 		if [ -f $SCRIPT -a -x $SCRIPT ]
-		  then
-        $SCRIPT
-			fi
+			then
+				$SCRIPT
+		fi
 	done
