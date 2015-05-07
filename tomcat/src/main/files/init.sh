fb4a768119355272a484759168428da3b1382a1a
Gordon Lawrenz <gordon.lawrenz@rwth-aachen.de>
2015-05-07 03:16:44 +0200

    Create init.sh
    File to trigger other scripts in /opt/init/

---
+++ tomcat/src/main/files/init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+INITDIR="/opt/init/"
+
+if [ ! `ls -A ${INITDIR}` ]
+  then
+    echo "empty (or does not exist)"
+    return 1
+  fi
+  
+for SCRIPT in ${INITDIR}*
+	do
+		if [ -f $SCRIPT -a -x $SCRIPT ]
+		  then
+        $SCRIPT
+			fi
+	done
