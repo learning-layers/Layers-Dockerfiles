e576f9ea9d090aba4b3c978053447d20f8d308d1
Gordon Lawrenz <gordon.lawrenz@rwth-aachen.de>
2015-05-08 21:30:29 +0200

    Update 40_configureTethysUserStorage.sh

--- tethys-userstorage/src/main/files/init/40_configureTethysUserStorage.sh
+++ tethys-userstorage/src/main/files/init/40_configureTethysUserStorage.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 # add ENV variables to /opt/tomcat/webapps/Tethys-UserStorage/src/main/resources/*.properties
+echo "Processing $0"
+echo "Configurating Tethys-UserStorage:"
+
 # TODO
+
+echo ""
