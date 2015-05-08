1c0c09d3b1f564a89553cd03695475efa5d565bd
Gordon Lawrenz <gordon.lawrenz@rwth-aachen.de>
2015-05-08 19:31:29 +0200

    Update init.sh

--- tomcat/src/main/files/init.sh
+++ tomcat/src/main/files/init.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+echo "Docker container started!"
+echo ""
+
 # Directory containing all init scripts. Those scripts need to be in a lexicographic order!
 if [ -z "${INIT_DIR}"]
 then
 	echo "INIT_DIR not set! Trying to use ${PWD}/init as INIT_DIR"
 	export INIT_DIR="${PWD}/init"
 fi
-# Check if INITDIR exists or is empty. If yes return error code 1.
+# Check if INIT_DIR exists or is empty. If yes return error code 1.
 if [ ! `ls -A ${INIT_DIR}` ]
 then
 	echo "empty (or does not exist)"
@@ -16,7 +19,7 @@ fi
 # Make every *.sh script executable. Doing this here prevents us of doing it in every Dockerfile based on this base image!
 chmod +x ${INIT_DIR}/*.sh 
 
-# Execute every executable file in IBNITDIR.
+# Execute every executable file in INIT_DIR.
 for SCRIPT in ${INIT_DIR}/*
 do
 	if [ -f ${SCRIPT} -a -x ${SCRIPT} ]
@@ -25,5 +28,6 @@ do
 	fi
 done
 
-# YAY! Everything worked! Return exit code 0!
-return 0
+# Return exit code 2 because we don't want our container to reach this line or it will stop!
+echo "Docker container finished processing scripts. Please contact maintainer as this container shouldn't finish!!!"
+return 2
