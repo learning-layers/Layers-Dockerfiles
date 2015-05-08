d01b05a4975ded8bc7b499f8b29eb9e289805594
Gordon Lawrenz <gordon.lawrenz@rwth-aachen.de>
2015-05-08 19:41:33 +0200

    Update 99_log.sh

--- tomcat/src/main/files/init/99_log.sh
+++ tomcat/src/main/files/init/99_log.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Follow "catalina.out"! This task should never end so the container can't exit!
+echo ""
+echo "Follow ${TOMCAT_HOME}/logs/catalina.out:"
 tail -f ${TOMCAT_HOME}/logs/catalina.out
