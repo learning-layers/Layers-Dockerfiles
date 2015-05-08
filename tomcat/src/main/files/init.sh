3c691134e4635763f2da3120bc147620f3d524cc
Gordon Lawrenz <gordon.lawrenz@rwth-aachen.de>
2015-05-08 21:14:03 +0200

    Update init.sh

--- tomcat/src/main/files/init.sh
+++ tomcat/src/main/files/init.sh
@@ -9,6 +9,7 @@ then
 	echo "INIT_DIR not set! Trying to use ${PWD}/init as INIT_DIR"
 	export INIT_DIR="${PWD}/init"
 fi
+
 # Check if INIT_DIR exists or is empty. If yes return error code 1.
 if [ ! `ls -A ${INIT_DIR}` ]
 then
@@ -19,8 +20,10 @@ fi
 # Make every *.sh script executable. Doing this here prevents us of doing it in every Dockerfile based on this base image!
 chmod +x ${INIT_DIR}/*.sh 
 
-# Execute every executable file in INIT_DIR.
 echo "Processing Scripts in ${INIT_DIR}"
+echo ""
+
+# Execute every executable file in INIT_DIR.
 for SCRIPT in ${INIT_DIR}/*
 do
 	if [ -f ${SCRIPT} -a -x ${SCRIPT} ]
