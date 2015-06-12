0323ef97459f345b440ab27a6b4fbb9116050db9
olhb <oliver.hildebrandt@gmail.com>
2015-06-12 15:27:59 +0200

    added schema upgrade

--- openidconnect/src/main/files/init/40_mysql.sh
+++ openidconnect/src/main/files/init/40_mysql.sh
@@ -2,3 +2,4 @@
 
 echo "The MYSQL pass is $OIDC_MYSQL_PASSWORD and $OIDC_MYSQL_USER"
 mysql -u$OIDC_MYSQL_USER -p$OIDC_MYSQL_PASSWORD -hmysql OpenIDConnect < /etc/sql/schema.sql
+mysql -u$OIDC_MYSQL_USER -p$OIDC_MYSQL_PASSWORD -hmysql OpenIDConnect < /etc/sql/schema_upgrade.sql
