83dbeca1f52a9d16e4c145f64aeaaa7dd8e3757a
jrwthll <manolov@dbis.rwth-aachen.de>
2015-06-24 14:53:01 +0200

    added a couple of services

--- backup.sh
+++ backup.sh
@@ -6,6 +6,7 @@ START=`date -Iseconds | sed -e "s/[-:]//g"` &&
 echo "Backup start time: $START" &&
 echo "" &&
 
+# does not currently work
 echo "Backing up Layers Common Data Storage MySQL databases volumes..." &&
 docker run --volumes-from mysql-data -v $(pwd):/backup learninglayers/base tar cvf ./backup/${START}-backup-mysql-data.tar /var/lib/mysql;
 echo " -> done" &&
@@ -15,6 +16,22 @@ echo "Backing up Layers Adapter data volumes..."
 docker run --volumes-from adapter-data -v $(pwd):/backup learninglayers/base tar cvf ./backup/${START}-backup-adapter-data.tar /usr/local/openresty/conf /usr/local/openresty/logs;
 echo " -> done" &&
 echo "" &&
+
+echo "Backing up Layers OpenLDAP data volumes..."
+docker run --volumes-from openldap-data -v $(pwd):/backup learninglayers/base tar cvf ./backup/${START}-backup-openldap-data.tar /usr/local/etc/openldap;
+echo " -> done" &&
+echo "" &&
+
+echo "Backing up Layers OpenID Connect data volumes..."
+docker run --volumes-from openidconnect-data -v $(pwd):/backup learninglayers/base tar cvf ./backup/${START}-backup-openidconnect-data.tar /etc/mitreid-connect /etc/sql;
+echo " -> done" &&
+echo "" &&
+
+echo "Backing up Layers OpenLDAP Account data volumes..."
+docker run --volumes-from openldapaccount-data -v $(pwd):/backup learninglayers/base tar cvf ./backup/${START}-backup-openldapaccount-data.tar /opt/conf /opt/account;
+echo " -> done" &&
+echo "" &&
+
 
 echo "Backing up Layers MobSOS Monitor data volumes..." &&
 docker run --volumes-from mobsos-monitor-data -v $(pwd):/backup learninglayers/base tar cvf ./backup/${START}-backup-mobsos-monitor-data.tar /opt/mobsos-monitor/etc /opt/mobsos-monitor/log;
