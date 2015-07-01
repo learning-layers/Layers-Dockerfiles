f01680ed431538d398d5ad8db63388461b23a555
Gordin <9ordin@gmail.com>
2015-07-01 16:17:20 +0200

    added openidconnect to docker-compose

--- openidconnect/src/main/files/init/40_mysql.sh
+++ openidconnect/src/main/files/init/40_mysql.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-echo "The MYSQL pass is $OIDC_MYSQL_PASSWORD and $OIDC_MYSQL_USER"
-mysql -u$OIDC_MYSQL_USER -p$OIDC_MYSQL_PASSWORD -hmysql OpenIDConnect < /etc/sql/schema.sql
+# Waiting for mysql server to become ready
+while ! ping -c1 mysql &>/dev/null; do sleep 0.2; done
+while ! timeout 1 bash -c 'cat < /dev/null > /dev/tcp/mysql/3306' 2>/dev/null; do sleep 0.5; done
+
+echo "The MYSQL pass is $OIDC_DB_PASS and $OIDC_DB_USER"
+mysql -u$OIDC_DB_USER -p$OIDC_DB_PASS -hmysql $OIDC_DB_NAME < /etc/sql/schema.sql
+
+while ! ping -c1 ldap &>/dev/null; do sleep 0.2; done
+while ! timeout 1 bash -c 'cat < /dev/null > /dev/tcp/ldap/389' 2>/dev/null; do sleep 0.5; done
