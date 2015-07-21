7e14a6ee1abc1cf9b4ec04808d02894d0df5b73a
Gordin <9ordin@gmail.com>
2015-07-21 02:48:22 +0200

    Merged changes from current deployment

--- compose.sh
+++ compose.sh
@@ -4,12 +4,12 @@ set -e
 
 mkdir -p mysql
 touch secret.env
-docker-compose stop -t 5 && sudo systemctl stop 'docker-*.scope'
+docker-compose stop -t 5
 ./backup.sh
 
 docker rm -f -v $(docker ps -a -f name=layersdockerfiles_ -q)
 bash ./setup_databases.sh
 
-docker-compose stop -t 5 && sudo systemctl stop 'docker-*.scope'
+docker-compose stop -t 5
 docker-compose up -d --x-smart-recreate
 docker-compose logs
