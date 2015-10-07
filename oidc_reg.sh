71678fbccb09b96d29fe0fc9fea1648e304b312e
olhb <Oliver.hildebrandt@gmail.com>
2015-10-07 11:50:23 +0200

    Update oidc_reg.sh

--- oidc_reg.sh
+++ oidc_reg.sh
@@ -5,12 +5,12 @@ chmod +x ./jq
 alias jq='./jq'
 
 
-OIDC_DATA=$(curl -X POST $LAYERS_API_URI/o/oauth2/register -d \
+OIDC_DATA=$(curl -X POST '$LAYERS_API_URI/o/oauth2/register' -d \
 "{
 	'application_type':'web',
 	'redirect_uris':[${REDIRECT_URIS}],
 	'client_name':'${CLIENT_NAME}'
-}" -H "Content-Type: application/json" -v)
+}" -H 'Content-Type: application/json' -v)
 
 CLIENT_ID=echo $OIDC_DATA | jq '.client_id' 
 CLIENT_SECRET=echo $OIDC_DATA | jq '.client_secret'
