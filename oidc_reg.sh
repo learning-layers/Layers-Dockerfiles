690314fd11e21d66a6d56dcc26a1975cca30d0a1
olhb <Oliver.hildebrandt@gmail.com>
2015-10-07 11:41:35 +0200

    Update oidc_reg.sh

--- oidc_reg.sh
+++ oidc_reg.sh
@@ -1,16 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 wget http://stedolan.github.io/jq/download/linux64/jq
 chmod +x ./jq
 alias jq='./jq'
 
 
-OIDC_DATA=`curl -X POST $LAYERS_API_URI/o/oauth2/register -d \
+OIDC_DATA=$(curl -X POST $LAYERS_API_URI/o/oauth2/register -d \
 "{
 	'application_type':'web',
 	'redirect_uris':[${REDIRECT_URIS}],
 	'client_name':'${CLIENT_NAME}'
-}" -H "Content-Type: application/json" -v `
+}" -H "Content-Type: application/json" -v)
 
 CLIENT_ID=echo $OIDC_DATA | jq '.client_id' 
 CLIENT_SECRET=echo $OIDC_DATA | jq '.client_secret'
